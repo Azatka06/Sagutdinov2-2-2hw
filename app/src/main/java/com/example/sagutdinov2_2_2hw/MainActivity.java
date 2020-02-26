223598b41c80854a8a45caf7a06cfff9424876f8
Azat Sagutdinov <azat.sagutdinov@tele2med.ru>
2020-02-26 14:57:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sagutdinov2_2_2hw/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.sagutdinov2_2_2hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+            Toast.makeText(MainActivity.this, "Отркыть записную книжку", Toast.LENGTH_LONG).show();
+            return true;
+
+        }
+        if (id == R.id.action_open_calendar) {
+            Intent intentCalendar = new Intent(MainActivity.this, CalendarActivity.class);
+            startActivity(intentCalendar);
+            Toast.makeText(MainActivity.this, "Отркыть Сроки задач", Toast.LENGTH_LONG).show();
+            return true;
+
+        }
+
+        if (id == R.id.action_open_spinner) {
+            Intent intentSpinner = new Intent(MainActivity.this, SpinnerActivity.class);
+            startActivity(intentSpinner);
+            Toast.makeText(MainActivity.this, "Отркыть Страны-Города-Улицы", Toast.LENGTH_LONG).show();
+            return true;
+
+        }
+
+        if (id == R.id.action_open_checkbox) {
+            Intent intentCheckbox = new Intent(MainActivity.this, CheckboxActivity.class);
+            startActivity(intentCheckbox);
+            Toast.makeText(MainActivity.this, "Отркыть ЧекБоксы", Toast.LENGTH_LONG).show();
+            return true;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
